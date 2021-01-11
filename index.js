3d51248aa4dcd8a7f22f7fa15eb846295896b14e
Christopher White <christopher.evan.white88@gmail.com>
2021-01-10 23:48:00 -0700

    added nodemon and standard packages and built a simple express server, and yes... it's very simple.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const { graphqlHTTP } = require('express-graphql')
+const gql = require('graphql-tag')
+const { buildASTSchema } = require('graphql')
+
+const app = express()
+app.use(cors())
+
+const schema = buildASTSchema(gql`
+  type Query {
+    hello: String
+  }
+`)
+
+const rootValue = {
+  hello: () => 'Hello, world!',
+}
+
+app.use('/graphql', graphqlHTTP({ schema, rootValue }))
+
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+  console.log(`Running graphql api server at localhost:${port}/graphql`)
+})
